39b96c4d41664f85e84b38e516d7d1cea71f025e
Giuseppe Lo Presti <glopresti@gmail.com>
2017-08-17 10:09:13 +0200

    Added automations dir to the sync process

--- qnap/refreshconfig.sh
+++ qnap/refreshconfig.sh
@@ -9,9 +9,11 @@
 # Giuseppe Lo Presti, July 2017
 ###############################################################
 
+PATH=/share/CACHEDEV1_DATA/.qpkg/container-station/bin:$PATH
 while true; do
   inotifywait --event modify ~glp/QSync/dev/DOMOTICA/homeass/*yaml
   cp ~glp/QSync/dev/DOMOTICA/homeass/*yaml /share/Container/homeass-config
+  cp ~glp/QSync/dev/DOMOTICA/homeass/automations/* /share/Container/homeass-config/automations
   # if running, restart the container to reflect the change
   id=`docker ps | grep homeass | awk '{print $1}'` && \
     docker restart $id
