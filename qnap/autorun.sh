238aa6524f5d2e0860eee5c83e0af3a265212b91
Giuseppe Lo Presti <glopresti@gmail.com>
2018-03-23 19:33:31 +0100

    Added tmux + moved zshrc content to a (static) flie

--- qnap/autorun.sh
+++ qnap/autorun.sh
@@ -6,6 +6,8 @@
 
 # set ssh tunnelling and sshd users
 sed -i 's/#PermitTunnel no/PermitTunnel yes/' /etc/ssh/sshd_config
+echo >> /etc/ssh/sshd_config
+echo 'PermitOpen any' >> /etc/ssh/sshd_config
 #sed -i 's/AllowUsers admin/AllowUsers admin glp/' /etc/ssh/sshd_config
 
 # keep monitoring and refreshing the home-assistant configuration
@@ -18,32 +20,19 @@ echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6xys2NBv//wVaLvxxG7h9ckwD/AmyEEBOcG
 # change shell to zsh
 usermod --shell /share/CACHEDEV1_DATA/.qpkg/Entware-ng/bin/zsh admin &> /dev/null
 
-# useful symbolic links
+# useful links to tools
 for s in ~admin/qnap/*sh; do ln -s -f $s /usr/local/bin; done
 ln -s /share/CACHEDEV1_DATA/.qpkg/Python3/python3 /usr/local/bin
 ln -s /opt/sbin/mosquitto /usr/local/sbin
 
-# zsh specific configuration
-cat > ~glp/.zshrc << EOF
-# path for qnapware utils and other stuff
-export PATH=$PATH:/Apps/sbin:/opt/bin:/Apps/opt/sbin:/usr/local/mariadb/bin:/share/CACHEDEV1_DATA/.qpkg/JRE/jre/bin:/share/CACHEDEV1_DATA/.qpkg/Python3/python3/bin:/share/CACHEDEV1_DATA/.qpkg/container-station/bin
-export PYTHONPATH=/share/CACHEDEV1_DATA/.qpkg/Python3/python3/lib/python3.5/site-packages
-
-# prompt
-export PROMPT=$'%{\e[32m%}%n%{\e[37m%}@%{\e[33m%}%m%{\e[37m%}:%{\e[36m%}%3/%{\e[0m%}> '
-export RPROMPT=$'%{\e[1;34m%}%D{%b %d}, %T%{\e[0m%}'
-
-# aliases
-alias ll='/bin/ls -laFh'
-alias nmapscan='nmap -sS -v -Pn -O'
-EOF
-
+# zsh and tmux specific configurations
 cp ~glp/.zshrc /root
+cp ~glp/.tmux.conf /root
 
-# useful links
+# useful links to common directories
 ln -s /share/CACHEDEV1_DATA/.qpkg /root/QTS
+ln -s /share/CACHEDEV1_DATA/Web /root/Web
 ln -s ~glp/QSync/qnap /root/qnap
 ln -s ~glp/QSync/dev/DOMOTICA /root/DOMOTICA
 ln -s /share/Container/homeass-config /root
 ln -s /share/Container/homeass-config/home-assistant.log /var/log
-
